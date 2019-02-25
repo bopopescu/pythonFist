b74511f175f4354e6fbc7982b67c5cba0fec05b4
wangyouzhan <wangyouzhan1987@163.com>
2019-02-25 21:54:04 +0800

    thread test

---
+++ src/asyncio/submit_task.py
@@ -0,0 +1,18 @@
+
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import time
+
+
+def action(max):
+    my_sum = 0
+    for i in range(max):
+        print(threading.current_thread().name + ' ' + str(i))
+        my_sum += i
+    return my_sum
+pool = ThreadPoolExecutor(max_workers=2)
+future1 = pool.submit(action, 50)
+future2 = pool.submit(action, 100)
+print(future1.done())
+pool.shutdown()
+
